4ff87cbd1a5ecc83e5347dd9967d58f5f9199cbd
Sekwah <contact@sekwah.com>
2024-08-29 21:47:01 +0200

    wip: fixing the translations and events [skip ci]
    Signed-off-by: Sekwah <contact@sekwah.com>

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,14 +1,7 @@
-import Vue from 'vue';
-import VueI18n from 'vue-i18n';
-
-Vue.use(VueI18n);
+import { createI18n } from 'vue-i18n';
 
 function loadLocaleMessages() {
-  const locales = require.context(
-    './locales',
-    true,
-    /[A-Za-z0-9-_,\s]+\.json$/i,
-  );
+  const locales = import.meta.glob('./locales/*.json');
   const messages = {};
   locales.keys().forEach((key) => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
@@ -55,7 +48,7 @@ function matchLocale(requestedLocale) {
   return 'en';
 }
 
-const i18n = new VueI18n({
+const i18n = createI18n({
   locale: matchLocale(
     (localStorage && localStorage.getItem('Locale')) ||
       navigator.language ||
